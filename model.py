4a6796db15be424dd3ce8925d0a8073726b1f79f
JVerbeek <janneke.verbeek@upcmail.nl>
2025-08-07 16:57:29 +0200

    fixed scoring

--- model.py
+++ model.py
@@ -1,40 +1,54 @@
 import abc
-import gpflow as gpf 
+import gpflow as gpf
 import numpy as np
-class Model(abc.ABC):
+
+
+class RegressionModel(abc.ABC):
     def __init__(self, model):
-        self.model = model 
-    
+        self.model = model
+        self.cusum_threshold = 0.1
+
     @abc.abstractmethod
-    def fit():
+    def fit(Xtrain, ytrain):
         pass
-    
+
     @abc.abstractmethod
-    def predict():
-        pass 
-    
-    
-class GPRModel(Model):
+    def predict(Xtest, ytest):
+        pass
+
+    @abc.abstractmethod
+    def predict_and_score(Xtest, ytest):
+        pass
+
+    def cusum_score(self, predictions, targets):
+        n = len(predictions)
+        m = len(targets)
+        cusum_score = np.cumsum((predictions - targets) ** 2 / m)
+        # changepoints = min(np.where(cusum_score > self.cusum_threshold)) / n
+        return cusum_score
+
+
+class GPRModel(RegressionModel):
     def __init__(self, model: gpf.models.BayesianModel):
         super().__init__(model)
-    
+
     def fit(self, X, y):
-        data = gpf.models.util.data_input_to_tensor((X.reshape(-1, 1), y.reshape(-1, 1)))
+        data = gpf.models.util.data_input_to_tensor(
+            (X.reshape(-1, 1), y.reshape(-1, 1)))
         self.model.data = data
         opt = gpf.optimizers.Scipy()
         opt.minimize(self.model.training_loss, self.model.trainable_variables)
-        return 
-    
+        return
+
     def predict(self, X_test, y_test, return_latent=False):
         fmean, fvar = self.model.predict_f(X_test)
         ymean, yvar = self.model.predict_y(X_test)
         if return_latent:
             return fmean, fvar, ymean, yvar
         return ymean, yvar
-        
-    def predict_and_compare(self, X_test, y_test):
-        return 42   
-    
-if __name__=="__main__":
-    mod = GPRModel(gpf.models.GPR((np.ones((2, 1)), np.ones((2, 1))), kernel=gpf.kernels.RBF()))
-    mod.fit(np.linspace(0, 1, 100).reshape(-1, 1), np.sin(0.1*np.linspace(0, 1, 100)).reshape(-1, 1))
+
+    def predict_and_score(self, X_test, y_test):
+        ymean, yvar = self.predict(X_test, y_test, return_latent=False)
+        score = self.cusum_score(ymean, y_test)
+        return score
+
---
+++ window_slide.py
@@ -0,0 +1,61 @@
+import numpy as np 
+from model import RegressionModel, GPRModel
+import gpflow as gpf
+import abc
+
+class Slider():   # Look ma, a strategy pattern! 
+    def __init__(self, regressor: RegressionModel, X, y):
+        self.regressor = regressor
+        self.data = (X, y)
+        self.data_length = len(X)
+
+    @abc.abstractmethod
+    def do_slide(self, timeseries, *args):
+        pass
+
+class MirrorSlider(Slider):
+    pass
+
+class NonOverlappingWindowSlider(Slider):
+    def __init__(self, regressor, X, y):
+        super().__init__(regressor, X, y)
+
+    def get_window_train_test(self, window_start, window_stop, window):
+        X, y = self.data
+        Xtrain = X[window_start:window_stop].reshape(-1, 1)
+        ytrain = y[window_start:window_stop].reshape(-1, 1)
+        Xtest = X[window_stop:window_stop+window].reshape(-1, 1)
+        ytest = y[window_stop:window_stop+window].reshape(-1, 1)
+        return Xtrain, Xtest, ytrain, ytest
+    
+    def do_slide(self, window_size):
+        window_start = 0
+        window_stop = window_size
+        scores = np.empty((10, 1))  # The first training window has no score.
+        
+        while window_stop < self.data_length + window_size:
+            Xtrain, Xtest, ytrain, ytest = self.get_window_train_test(window_start, window_stop, window_size)
+            self.regressor.fit(Xtrain, ytrain)
+            score = self.regressor.predict_and_score(
+                Xtest, ytest).reshape(-1, 1)
+            scores = np.concatenate((scores, score))
+            
+            window_start = window_stop   # Update window
+            window_stop = window_start + window_size
+            
+        return scores
+
+if __name__ == "__main__":
+    data = np.load("test_change_type.npz")
+    X, y = data["Xs"][42], data["ys"][42]
+    mod = GPRModel(gpf.models.GPR(
+        (np.ones((2, 1)), np.ones((2, 1))), kernel=gpf.kernels.RBF()))
+    
+    slider = NonOverlappingWindowSlider(mod, X, y)
+    scores = slider.do_slide(10)
+   
+    import matplotlib.pyplot as plt 
+    plt.plot(X, y)
+    plt.plot(X, scores)
+    plt.show()
+    
