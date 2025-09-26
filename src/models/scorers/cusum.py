4e7d31d2c97b7d98b6291f44e485b9bfe057ddc6
Roel Bouman <roel.bouman@ru.nl>
2025-09-26 15:18:04 +0200

    Updated cusum to take predictions into account as expectation

--- src/models/scorers/cusum.py
+++ src/models/scorers/cusum.py
@@ -2,12 +2,18 @@ import numpy as np
 
 class BidirectionalCUSUMScorer:
     def __init__(self, decay=0):
+        """
+        Initialize the BidirectionalCUSUMScorer with a decay parameter.
+        Args:
+            decay (float): The constant decay to apply at each timestep.
+        """
         self.decay = decay
 
-    def score(self, predictions_list):
+    def score(self, X_s, predictions_list):
         """
         Aggregate a list of prediction arrays using bidirectional CUSUM.
         Args:
+            X_s (list of np.ndarray): List of input data arrays corresponding to predictions.
             predictions_list (list of np.ndarray): List of prediction arrays from different models.
         Returns:
             np.ndarray: Aggregated predictions.
@@ -15,14 +21,15 @@ class BidirectionalCUSUMScorer:
         if not predictions_list:
             raise ValueError("predictions_list cannot be empty.")
 
-        scores_list = [self._score_array(preds) for preds in predictions_list]
+        scores_list = [self._score_array(X, preds) for X, preds in zip(X_s, predictions_list)]
 
         return scores_list
 
-    def _score_array(self, predictions):
+    def _score_array(self, X, predictions):
         """
         Apply CUSUM scoring to the predictions.
         Args:
+            X (np.ndarray): Input data array.
             predictions (np.ndarray): Array of model predictions.
         Returns:
             np.ndarray: CUSUM scores.
@@ -31,9 +38,9 @@ class BidirectionalCUSUMScorer:
         cusum_pos = 0
         cusum_neg = 0
 
-        for i, pred in enumerate(predictions):
-            cusum_pos = max(0, cusum_pos + pred - self.decay)
-            cusum_neg = min(0, cusum_neg + pred + self.decay)
+        for i, (x, pred) in enumerate(zip(X,predictions)):
+            cusum_pos = max(0, cusum_pos + (x - pred) - self.decay)
+            cusum_neg = min(0, cusum_neg + (x - pred) + self.decay)
             scores[i] = max(cusum_pos, -cusum_neg)
 
         return scores
@@ -42,10 +49,11 @@ class PositiveCUSUMScorer:
     def __init__(self, decay=0):
         self.decay = decay
 
-    def score(self, predictions_list):
+    def score(self, X_s, predictions_list):
         """
         Aggregate a list of prediction arrays using positive CUSUM.
         Args:
+            X_s (list of np.ndarray): List of input data arrays corresponding to predictions.
             predictions_list (list of np.ndarray): List of prediction arrays from different models.
         Returns:
             np.ndarray: Aggregated predictions.
@@ -53,14 +61,15 @@ class PositiveCUSUMScorer:
         if not predictions_list:
             raise ValueError("predictions_list cannot be empty.")
 
-        scores_list = [self._score_array(preds) for preds in predictions_list]
+        scores_list = [self._score_array(X, preds) for X, preds in zip(X_s, predictions_list)]
 
         return scores_list
 
-    def _score_array(self, predictions):
+    def _score_array(self, X, predictions):
         """
         Apply positive CUSUM scoring to the predictions.
         Args:
+            X (np.ndarray): Input data array.
             predictions (np.ndarray): Array of model predictions.
         Returns:
             np.ndarray: Positive CUSUM scores.
@@ -68,8 +77,8 @@ class PositiveCUSUMScorer:
         scores = np.zeros_like(predictions)
         cusum_pos = 0
 
-        for i, pred in enumerate(predictions):
-            cusum_pos = max(0, cusum_pos + pred - self.decay)
+        for i, (x, pred) in enumerate(zip(X, predictions)):
+            cusum_pos = max(0, cusum_pos + (x - pred) - self.decay)
             scores[i] = cusum_pos
 
         return scores
@@ -78,10 +87,11 @@ class NegativeCUSUMScorer:
     def __init__(self, decay=0):
         self.decay = decay
 
-    def score(self, predictions_list):
+    def score(self, X_s, predictions_list):
         """
         Aggregate a list of prediction arrays using negative CUSUM.
         Args:
+            X_s (list of np.ndarray): List of input data arrays corresponding to predictions.
             predictions_list (list of np.ndarray): List of prediction arrays from different models.
         Returns:
             np.ndarray: Aggregated predictions.
@@ -89,14 +99,15 @@ class NegativeCUSUMScorer:
         if not predictions_list:
             raise ValueError("predictions_list cannot be empty.")
 
-        scores_list = [self._score_array(preds) for preds in predictions_list]
+        scores_list = [self._score_array(X, preds) for X, preds in zip(X_s, predictions_list)]
 
         return scores_list
 
-    def _score_array(self, predictions):
+    def _score_array(self, X, predictions):
         """
         Apply negative CUSUM scoring to the predictions.
         Args:
+            X (np.ndarray): Input data array.
             predictions (np.ndarray): Array of model predictions.
         Returns:
             np.ndarray: Negative CUSUM scores.
@@ -104,8 +115,8 @@ class NegativeCUSUMScorer:
         scores = np.zeros_like(predictions)
         cusum_neg = 0
 
-        for i, pred in enumerate(predictions):
-            cusum_neg = min(0, cusum_neg + pred + self.decay)
+        for i, (x, pred) in enumerate(zip(X, predictions)):
+            cusum_neg = min(0, cusum_neg - (x - pred) + self.decay)
             scores[i] = -cusum_neg
 
         return scores
