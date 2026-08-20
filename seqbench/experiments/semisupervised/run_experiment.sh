1da65792d7a9b5420b1c13059e1215e4c7165d29
JVerbeek <janneke.verbeek@upcmail.nl>
2026-08-20 14:16:19 +0200

    Restructured experiments, now run from shell script.

--- seqbench/experiments/semisupervised/run_experiment.sh
+++ seqbench/experiments/semisupervised/run_experiment.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=das
+#SBATCH --qos=das-small
+#SBATCH --cpus-per-task= 4
+#SBATCH --mem=10G
+#SBATCH --gres=gpu:1
+#SBATCH --time= 10:00:00
+#SBATCH --output=%A_%a.out
+#SBATCH --error=%A-%a.err
+#SBATCH --array=0-1
+
 python3 ../../masterscript.py --generator-hyperparameters experiment-config/data_config.yaml \
     --window-slider-kwargs ../../config/window_slider.yaml\
     --regressor pyseq.models.regressors.random_forest_regression.MultiOutputRandomForest\
